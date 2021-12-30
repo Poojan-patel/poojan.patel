6ac9fe52e913c33fd106cb62f7ee7a0555b9225f
Poojan Patel <poojan4004@gmail.com>
2021-12-30 22:32:28 +0530

    Initial Commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react'
+import "./intro.scss"
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import {init} from 'ityped'
+
+export default function Intro() {
+
+     const textRef = useRef();
+
+     useEffect(() => {
+          console.log(textRef)
+          init(textRef.current,{
+               showCursor: true,
+               strings: ["Developer","Designer","Learner"],
+               backDelay: 1000
+          })
+     }, [])
+
+     return (
+          <div className="intro" id="intro">
+               
+               <div className="right">
+                    <div className="wrapper">
+                         <h2>Hi There, This is</h2>
+                         <h1>Poojan Patel</h1>
+                         <h3><span ref={textRef}></span></h3>
+                    </div>
+                    <a href="#portfolio">
+                         <KeyboardArrowDownIcon className="down-arrow" />
+                    </a>
+               </div>
+               <div className="left">
+                    <div className="imgContainer">
+                         <img src="assets/download.png" alt=""></img>
+                    </div>
+               </div>
+          </div>
+     )
+}
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,11 @@
+import "./portfolioList.scss"
+
+import React from 'react'
+
+export default function PortfolioList({title}) {
+     return (
+          <div className="portfolioList">
+               {title}
+          </div>
+     )
+}
