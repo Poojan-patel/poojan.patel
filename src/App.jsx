2c6f692f7687182656ab4415404950f5810cd4ea
Poojan Patel <56884359+Poojan-patel@users.noreply.github.com>
2023-11-17 12:52:56 +0530

    Experience Module order changed (#2)
    Co-authored-by: Poojan Patel <poojan@tekion.com>

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
       <div className="sections">
         <Intro/>
         <Skills/>
-        <Projects/>
         <Experience/>
+        <Projects/>
         <Contact/>
       </div>
     </div>
